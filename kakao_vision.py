14f0cfbe4878afd8bd71a447d3ff3918a39d5fd2
krepe-suZette <37508580+krepe-suZette@users.noreply.github.com>
2018-10-10 02:03:33 +0900

    Reduce unnecessary vision api call (API limit has been exceeded.)

--- kakao_vision.py
+++ kakao_vision.py
@@ -5,6 +5,13 @@ import requests
 
 API_URL = 'https://kapi.kakao.com/v1/vision/adult/detect'
 APP_KEY = ''
+API_STATUS = 1
+API_STATUS_UPDATE = datetime.date.today()
+ERROR_MSG = {
+    -10: "오늘 최대 이용횟수를 초과했습니다. 내일 다시 시도해주세요.",
+    -603: "이미지 로딩 시간초과. 더 작은 크기의 이미지를 사용해주세요.",
+    -911: "지원하지 않는 이미지 포맷입니다."
+}
 
 
 def _save_image(url):
@@ -19,7 +26,21 @@ def _save_image(url):
 
 def detect_adult(image_url):
     _save_image(image_url)
+
+    global API_STATUS
+    print(API_STATUS)
+    if datetime.date.today() != API_STATUS_UPDATE:
+        API_STATUS = 1
+    elif API_STATUS == 1:
+        pass
+    else:
+        return {'msg': '오늘 최대 이용횟수를 초과했습니다. 내일 다시 시도해주세요.'}
     headers = {'Authorization': f'KakaoAK {APP_KEY}'}
     data = {'image_url': image_url}
     resp = requests.post(API_URL, headers=headers, data=data)
-    return resp.json()
+    result = resp.json()
+    if resp.status_code == 400:
+        result['msg'] = ERROR_MSG.get(result['code'], result['msg'])
+        if result['code'] == -10:
+            API_STATUS = 0
+    return result
